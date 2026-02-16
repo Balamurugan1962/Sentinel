377febee0506a605324d8d19e0772963f96cc8ae
Balamurugan S <balamurugan.sekar@icloud.com>
2026-02-16 22:21:56 +0530

    chore(client): remove target

--- client/poc-rust/target/debug/build/serde-bd66712cb0deb479/build_script_build-bd66712cb0deb479.d
+++
@@ -1,5 +0,0 @@
-D:\Sentinel\client\poc-rust\target\debug\build\serde-bd66712cb0deb479\build_script_build-bd66712cb0deb479.d: C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-D:\Sentinel\client\poc-rust\target\debug\build\serde-bd66712cb0deb479\build_script_build-bd66712cb0deb479.exe: C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs:
--- client/poc-rust/target/debug/build/serde_core-0065990d19f7f46a/build_script_build-0065990d19f7f46a.d
+++
@@ -1,5 +0,0 @@
-D:\Sentinel\client\poc-rust\target\debug\build\serde_core-0065990d19f7f46a\build_script_build-0065990d19f7f46a.d: C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-D:\Sentinel\client\poc-rust\target\debug\build\serde_core-0065990d19f7f46a\build_script_build-0065990d19f7f46a.exe: C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-C:\Users\Rahul\ V\ S\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs:
