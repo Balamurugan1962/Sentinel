da1e5bf870e161c3ec96ca96dd99ffc61eccd74f
Balamurugan S <balamurugan.sekar@icloud.com>
2026-03-10 19:08:44 +0530

    feat: browser logger

---
+++ client/sentry-extenstion/background.js
@@ -0,0 +1,24 @@
+function sendLog(url) {
+  fetch("http://127.0.0.1:7777/log", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: url,
+      timestamp: Date.now(),
+    }),
+  }).catch((err) => console.log("log failed", err));
+}
+
+// Fires on normal page loads
+chrome.webNavigation.onCompleted.addListener((details) => {
+  if (details.frameId !== 0) return;
+  sendLog(details.url);
+});
+
+// Fires when SPA updates URL (YouTube, Twitter, etc.)
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  if (details.frameId !== 0) return;
+  sendLog(details.url);
+});
--- client/sentry/src/monitor/browser.rs
+++ client/sentry/src/monitor/browser.rs
@@ -1,31 +1,129 @@
+use axum::extract::State;
 use axum::{routing::post, Json, Router};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::ClientConfig;
 use serde::Deserialize;
+use serde_json::json;
+use std::io::Write;
+use std::sync::Arc;
+use std::time::Duration;
+use std::{fs, path::Path};
 use tokio::{net::TcpListener, sync::broadcast};
+use tower_http::cors::{Any, CorsLayer};
 
 // TODO:
 // need to share this to kafka
 #[derive(Deserialize)]
 struct Visit {
     url: String,
-    _timestamp: u64,
+    timestamp: u64,
 }
 
-fn enforce_policy() {} // need to implement policy enfocement in firefox
+struct AppState {
+    producer: FutureProducer,
+}
 
-async fn log(Json(data): Json<Visit>) {
-    println!("Visited: {}", data.url); // need to send this log to kafka
+fn create_kafka_producer(kafka_ip: &str) -> FutureProducer {
+    ClientConfig::new()
+        .set("bootstrap.servers", kafka_ip)
+        .create()
+        .expect("Failed to create Kafka producer")
+}
+
+// Need to test and ensure this
+fn enforce_firefox_policy() {
+    if !Path::new("/usr/bin/firefox").exists() {
+        println!("Firefox not installed, skipping policy.");
+        return;
+    }
+
+    let dir = "/etc/firefox/policies";
+    let path = format!("{}/policies.json", dir);
+
+    fs::create_dir_all(dir).unwrap();
+
+    let policy = r#"
+{
+  "policies": {
+    "DisableDeveloperTools": true,
+    "DNSOverHTTPS": { "Enabled": false },
+    "Extensions": {
+      "Install": [
+        "https://your-server/monitor.xpi"
+      ],
+      "Locked": [
+        "monitor@sentinel"
+      ]
+    }
+  }
+}
+"#;
+
+    let mut file = fs::File::create(path).unwrap();
+    file.write_all(policy.as_bytes()).unwrap();
+
+    println!("Firefox policy enforced");
+}
+
+fn enforce_policy() {
+    #[cfg(target_os = "linux")]
+    enforce_firefox_policy();
+}
+
+async fn log(State(state): State<Arc<AppState>>, Json(data): Json<Visit>) {
+    println!("Visited: {}", data.url);
+
+    let payload = json!({
+        "url": data.url,
+        "timestamp": data.timestamp
+    })
+    .to_string();
+
+    let _ = state
+        .producer
+        .send(
+            // Topic: [Client_id]-log-browser
+            FutureRecord::to("[client_id]-browser")
+                .payload(&payload)
+                .key("visit"),
+            Duration::from_secs(0),
+        )
+        .await;
 }
 
 pub async fn browser_monitor(_shutdown_tx: broadcast::Receiver<()>) {
+    // TODO:
+    // need to hook it with config
+
+    let kafka_ip = "127.0.0.1";
+    let producer = create_kafka_producer(kafka_ip);
+
+    let state = Arc::new(AppState { producer });
+
     // before starting the system, need to enforce the
     // policy to the firefox, for this testing we need to do in ubuntu
 
-    let app = Router::new().route("/log", post(log));
+    // enforce_policy();
+
+    // TODO:
+    // change cors allowed origins
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let app = Router::new()
+        .route("/log", post(log))
+        .layer(cors)
+        .with_state(state);
+
     // should always run in localhost
     // TODO:
     // need to make it secure. either by some kinda key or integrity check
     // but anyways its used to logs, well see.
     let listener = TcpListener::bind("127.0.0.1:7777").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
